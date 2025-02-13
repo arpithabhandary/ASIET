f319995f5b05f6718ba9a461ecf9209e0a723de0
Arpitha Bhandary <149690481+arpithabhandary@users.noreply.github.com>
2025-02-13 11:51:07 +0530

    FCFS  array

--- os/fcfs.c
+++ os/fcfs.c
@@ -1,54 +1,83 @@
-#include<stdio.h>
-#include<string.h>
-typedef struct process{
-	int pid;
-	char p[10];
-	int AT;
-	int BT;
-}fcfs;
-
-void getprocess(fcfs p[100],int n)
+#include <stdio.h>
+void main()
+{
+	int i,n;
+	int p[100],wt[100],at[100],bt[100],tt[100];
+	float avgwt=0,avgtt=0;
+	printf("Enter number of process:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
 	{
-	int i;
-	for(i=0;i<n;i++){
-	printf("enter the %d Process PID",i+1);
-	scanf("%d",&p[i].pid);
-	printf("Enter the arrival time ");
-	scanf("%d",&p[i].AT);
-	printf("enter the Burst Time");
-	scanf("%d",&p[i].BT);
+		printf("Enter the process ID:");
+		scanf("%d",&p[i]);
+		printf("Enter burst time:");
+		scanf("%d",&bt[i]);
 	}
-}
-void sort(struct process p[100],int n)
-{struct process temp;
- 	
-	for(int i=0;i<n-1;i++)
+	wt[0]=0;
+	for(i=1;i<n;i++)
 	{
-		for(int j=0;j<n-i-1;j++)
-		{
-	if(p[j].AT>p[j+1].AT){
-		temp=p[j];
-		p[j]=p[j+1];
-		p[j+1]=temp;
-		}
+		wt[i] = bt[i-1] + wt[i-1];
+		avgwt += wt[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		tt[i] = wt[i] + bt[i];
+		avgtt += tt[i];
+		
+	}
 	
+	printf("Process Id\tBurst Time\tWaiting Time\tTurn Around Time\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t\t%d\t\t%d\t\t%d\n",p[i],bt[i],wt[i],tt[i]);
 	}
+	printf("Average Waiting Time:%f\n",avgwt/n);
+	printf("Average Turn Around Time:%f\n",avgtt/n);
+	
+	printf("\nGANT CHART\n");
+	printf("----------------------------------------------------------------------------------\n");
+	for(i=0;i<n;i++)
+	{
+		printf("|\tP%d\t|",p[i]);
+	}
+	printf("\n");
+	printf("----------------------------------------------------------------------------------\n");
+	printf("0\t\t");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t\t",tt[i]);
+	}
+	printf("\n");
+	
+	
 }
 
-}
-void printsortedprocesses(struct process p[100],int n){
-printf("PID\tArrivalTime\tBurstTime\n");
-for(int i=0;i<n;i++){
-printf("%d\t%d\t\t%d\n",p[i].pid,p[i].AT,p[i].BT);
-}
-}
-int main(){
-int n;
-printf("ente the no of processcor");
-scanf("%d",&n);
-struct process p[100];
-getprocess(p,n);
-sort(p,n);
-printsortedprocesses(p,n);
-return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
